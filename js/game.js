ff2074cc0d56287323f2d23e88325d9741f1f456
nuno-nogueira <nunonogueira05@gmail.com>
2024-05-14 22:42:04 +0100

    Escape Room Page - HTML & CSS

--- js/game.js
+++ js/game.js
@@ -43,10 +43,11 @@ function countdown(minutes, seconds){
 // MODAL
 
 let modal = document.getElementById('quiz_modal');
+modal.style.display = 'none';
 let closeButton = document.getElementsByClassName('close')[0];
 function openModal() {
     modal.style.display = 'block';
-    displayNextQuestion();
+    // displayNextQuestion();
 }
 function closeModal() {
     modal.style.display = 'none';
--- js/models/example.js
+++ js/models/example.js
@@ -1,2 +1,4 @@
 //entity models (classes)
 //manage data
+let allparagraphs = document.getElementsByTagName('subtitle').innerHTML
+console.log(allparagraphs)
