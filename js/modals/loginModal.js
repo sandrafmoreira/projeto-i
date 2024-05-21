30b7e959551bdfc00dc4b4e923891f33de29f7ea
sandrafmoreira <sandramoreira.im@gmail.com>
2024-05-21 11:12:49 +0100

    update modals

--- js/modals/loginModal.js
+++ js/modals/loginModal.js
@@ -1,25 +1,31 @@
-document.getElementById("open-login-modal").addEventListener("click", function(event) {
-  event.preventDefault(); //evita a ação do link querer abrir uma nova pagina
-  openLoginModal();
-});
+document.addEventListener("DOMContentLoaded", () => {
+  let openLoginModalLink = document.getElementById("open-login-modal");
 
-function openLoginModal() {
-  let loginModal = document.getElementById('login-modal-div');
-  loginModal.style.display = 'block'; //fazer aparecer
+  openLoginModalLink.addEventListener("click", function(event) {
+    event.preventDefault(); //evita a ação do link querer abrir uma nova pagina
+    openLoginModal();
+  });
 
-  //upload do conteudo da modal que esta noutra pag, para a div loginModalContent
-  fetch('./html/login.html') //obter a pagina
-      .then(loginPage => loginPage.text()) //obter o body em string
-      .then(content => {
-          document.getElementById("loginModalContent").innerHTML = content;
-      })
-}
+  function openLoginModal() {
+    let loginModal = document.getElementById('login-modal-div');
+    loginModal.style.display = 'block'; //fazer aparecer
 
-let btnCloseLogin = document.querySelector(".close_modal_btn");
-btnCloseLogin.addEventListener("click", function() {
-  let loginModal = document.getElementById('login-modal');
-  loginModal.style.display = 'none';
-});
+    //upload do conteudo da modal que esta noutra pag, para a div loginModalContent
+    fetch('./html/login.html') //obter a pagina
+        .then(loginPage => loginPage.text()) //obter o body em string
+        .then(content => {
+            document.getElementById("loginModalContent").innerHTML = content;
+        })
+  }
+
+  // let btnCloseLogin = document.querySelector(".close_modal_btn");
+  // btnCloseLogin.addEventListener("click", function() {
+  //   let loginModal = document.getElementById('login-modal');
+  //   loginModal.style.display = 'none';
+  // });
+})
+
+
 
 
 
---
+++ js/views/navbarView.js
@@ -0,0 +1,22 @@
+
+function navbarView() {
+    let result = `
+        <div class="nav-width white-nav">
+            <a href="./html/about.html">SOBRE NÓS</a>
+            <a href="https://www.esmad.ipp.pt/" target="_blank">ESMAD</a>
+            <a href="./html/course.html">CURSO</a>
+            <a href="#">
+                <img src="./assets/logo.png" alt="tsiw logo" class="nav-logo">
+            </a>
+            <div class="nav-esc">
+                <a href="./html/game.html">MAZE</a>
+                <img src="./assets/index/Labyrinth.png" alt="tsiw logo" class="nav-maze">
+            </div>
+            <a href="./html/login.html" id="open-login-modal" class="nav-login-btn">INICIAR SESSÃO</a>
+            <div class="nav-orange-divider"></div>
+            <a href="./html/sign-up.html">CRIAR CONTA</a>
+        </div>
+    `
+
+    
+}
