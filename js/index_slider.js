be4f5723e1c164386973890ede3624d0ce106d1f
kenLukau31 <kenlukau@gmail.com>
2024-05-31 00:59:01 +0100

    slide Show index js done

--- js/index_slider.js
+++ js/index_slider.js
@@ -21,23 +21,53 @@
 
 
 const carousel = document.querySelector(".carousel");
+const icons = document.querySelectorAll(".wrapper i")
+const firstImag = document.querySelectorAll("img")[0]
 
-let isDragStart = false, prevpageX, prevscrollleft;
+
+let isSliderStart = false, prevpageX, prevscrollleft;
+let firstImgWidth =firstImag.clientWidth + 8;
+console.log(firstImgWidth)
+
+icons.forEach(function(icon) {
+    
+    icon .addEventListener("click",()=>{
+        console.log(icon)
+        if (icon.id == "left"){
+            carousel.scrollLeft -= firstImgWidth
+        } else{
+            carousel.scrollLeft += firstImgWidth
+        }
+    })
+})
+
+
 
 
 const dragstart= (e) => {
-    isDragStart = true
+    // verificar se o movimento começou
+    isSliderStart = true 
+
+    //  posição inicial do mouse no inicio do movimento  
     prevpageX= e.pageX;
+
+    // posição inicial do scroll
     prevscrollleft= carousel.scrollLeft;
+    
 }
 const dragstop = () => {
-    isDragStart = false
+    // verificar se o movimento parou
+    isSliderStart = false
 }
 
 const dragging= (e) => {
-   if (!isDragStart)return;
+   if (!isSliderStart)return;
     e.preventDefault();
+    // diferençã entra a posição atual e inical do mouse
     let positiondiff= e.pageX - prevpageX;
+
+    // diferençã entra a posição atual e inical do scroll de forma amover no sentido contrario do mouse 
+    // e dar a sensasão de arrasto
     carousel.scrollLeft = prevscrollleft-positiondiff;
     console.log(e.pageX)
 }
