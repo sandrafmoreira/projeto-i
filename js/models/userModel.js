468276a93483e561d268782ad60f812e349f5f6c
sandrafmoreira <sandramoreira.im@gmail.com>
2024-05-30 16:09:54 +0100

    criar conta

--- js/models/userModel.js
+++ js/models/userModel.js
@@ -1,68 +1,68 @@
 /**
  * CLASSE QUE MODELA UM UTILIZADOR NA APLICAÇÃO
  */
-export default class User {
-    email = "";
-    password = "";
-    
-    constructor(email, password) {
+export class User {
+
+    constructor(name, surname, email, password) {
+        this.name = name;
+        this.surname = surname;
         this.email = email;
         this.password = password;
-        
     }
+
 }
 
 let users = [];
-let admin1 = new User("sandra@123", "123");
-users.push(admin1);
-let admin2 = new User("nuno@123", "123");
-users.push(admin1);
-let admin3 = new User("ken@123", "123");
+let admin1 = new User("Sandra", "Moreira", "sandra@123", "123");
+let admin2 = new User("Nuno", "Nogueira", "nuno@123", "123");
+let admin3 = new User("Ken", "Lukau", "ken@123", "123");
 users.push(admin1, admin2, admin3);
 console.log(users);
 
-// Adicionar à local storage
-localStorage.setItem("users", JSON.stringify(users));
-
-// Descarregar utilizadores da Localstorage - function init()
+// descarregar utilizadores da local storage:
 export function init() {
     if(localStorage.users) {
         const tempUsers = JSON.parse(localStorage.users);
         for(let user of tempUsers) {
-            users.push(new User(user.email, user.password))
+            users.push(new User(user.name, user.surname, user.email, user.password))
         }
-        console.log("users loaded:", users);
-    } else {
-        users = [];
-        console.log("no users in local storage");
     }
 }
 
-// Descarregar user que esta a fazer login da local storage
+// descarregar user que esta a fazer login da local storage:
 export function login(email, password) {
-    console.log("trying to log in with email:", email, "and password:", password);
-    const user = users.find((user) =>
+    const user = users.find(user =>
         user.email == email && user.password == password
     ); 
-    console.log("user:", user);
     if (user) {
         sessionStorage.setItem("loggedUser", JSON.stringify(user));
-        console.log("User logged in.");
-        return true;
     } else {
-        console.log("Login failed. User not found or credentials mismatch.");
+        console.log("User not found.");
     }
 }
 
-// USER logout
+// verificar se o user existe:
+export function isLogged() {
+    return sessionStorage.getItem("loggedUser") ? true : false;
+  }
+
+// logout:
 export function logout() {
     sessionStorage.removeItem("loggedUser");
 }
 
-// verificar se o user existe
-export function isLogged() {
-    return sessionStorage.getItem("loggedUser") ? true : false;
-  }
-  
-  
-
+// adicionar conta:
+export function add(name, surname, email, password) {
+    if(users.some(user => user.email === email)) {
+        alert("Já existe uma conta com esse email.")
+        throw Error(`Já existe uma conta com esse email.`);
+    } else {
+        users.push(
+            new User(name, surname, email, password)
+        )
+        // atualizar a local storage:
+        localStorage.setItem("users", JSON.stringify(users));
+        alert("blablabla")
+    }
+    console.log(users);
+}
---
+++ js/sign-up.js
@@ -0,0 +1,22 @@
+import * as User from "./models/userModel.js"
+
+User.init();
+
+// criar conta ao preencher o form:
+document.querySelector(".sign_form")?.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const signUpName = document.getElementById("signUpName").value;
+    const signUpSurname = document.getElementById("signUpSurname").value;
+    const signUpEmail = document.getElementById("signUpEmail").value;
+    const signUpPassword = document.getElementById("signUpPassword").value;
+    const signUpConfirmPassword = document.getElementById("signUpConfirmPassword").value;
+
+    if (signUpPassword !== signUpConfirmPassword) {
+        console.log(signUpPassword.value, signUpConfirmPassword.value);
+        alert("Confirme a palavra-passe.")
+    } else if (signUpPassword == signUpConfirmPassword) {
+        User.add(signUpName, signUpSurname, signUpEmail, signUpPassword);
+        alert("Conta registada com sucesso!")
+    }
+})
